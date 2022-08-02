c740307949b1bab5577c57482eb6d17c81e3f14c
Eduardo Viega <eduardoviega12@gmail.com>
2022-08-02 10:54:37 -0300

    Selects

--- 5- Conceitos bancos de dados PostgreSQL/selects.sql
+++ 5- Conceitos bancos de dados PostgreSQL/selects.sql
@@ -1,18 +1,18 @@
-SELECT * FROM agencia
-SELECT * FROM banco
-SELECT * FROM cliente
-SELECT * FROM cliente_transacoes
-SELECT * FROM conta_corrente
-SELECT * FROM tipo_transacao
+SELECT * FROM agencia;
+SELECT * FROM banco;
+SELECT * FROM cliente;
+SELECT * FROM cliente_transacoes;
+SELECT * FROM conta_corrente;
+SELECT * FROM tipo_transacao;
 
-SELECT banco_numero, numero, nome, ativo, data_criacao FROM agencia
-SELECT numero, nome, ativo, data_criacao FROM banco
-SELECT numero, nome, email, ativo, data_criacao FROM cliente
+SELECT banco_numero, numero, nome, ativo, data_criacao FROM agencia;
+SELECT numero, nome, ativo, data_criacao FROM banco;
+SELECT numero, nome, email, ativo, data_criacao FROM cliente;
 SELECT id, banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor, data_criacao 
-FROM cliente_transacoes
+FROM cliente_transacoes;
 SELECT banco_numero, agencia_numero, numero, digito, cliente_numero, ativo, data_criacao 
-FROM conta_corrente
-SELECT id, nome, ativo, data_criacao FROM tipo_transacao
+FROM conta_corrente;
+SELECT id, nome, ativo, data_criacao FROM tipo_transacao;
 
 -- Colunas de uma tabela
 SELECT column_name, data_type, character_maximum_length 
@@ -25,49 +25,49 @@ WHERE table_name = 'cliente_transacoes';
 -- MIN
 -- SUM
 
-SELECT valor FROM cliente_transacoes
+SELECT valor FROM cliente_transacoes;
 
-SELECT AVG(valor) FROM cliente_transacoes -- Média
+SELECT AVG(valor) FROM cliente_transacoes; -- Média
 
-SELECT COUNT(numero) FROM cliente
+SELECT COUNT(numero) FROM cliente;
 
 SELECT COUNT(numero), email
 FROM cliente
 WHERE email ILIKE '%@gmail.com'
-GROUP BY email
+GROUP BY email;
 
-SELECT MAX(numero) FROM cliente
-SELECT MIN(numero) FROM cliente
+SELECT MAX(numero) FROM cliente;
+SELECT MIN(numero) FROM cliente;
 
-SELECT MAX(valor) FROM cliente_transacoes
-SELECT MIN(valor) FROM cliente_transacoes
+SELECT MAX(valor) FROM cliente_transacoes;
+SELECT MIN(valor) FROM cliente_transacoes;
 
 SELECT MAX(valor), tipo_transacao_id
 FROM cliente_transacoes
-GROUP BY tipo_transacao_id
+GROUP BY tipo_transacao_id;
 
 SELECT MIN(valor), tipo_transacao_id
 FROM cliente_transacoes
-GROUP BY tipo_transacao_id
+GROUP BY tipo_transacao_id;
 
 SELECT COUNT(id), tipo_transacao_id
 FROM cliente_transacoes
 GROUP BY tipo_transacao_id
-HAVING COUNT(id) > 150
+HAVING COUNT(id) > 150;
 
 SELECT SUM(valor)
+FROM cliente_transacoes;
+
+SELECT SUM(valor), tipo_transacao_id
 FROM cliente_transacoes
+GROUP BY tipo_transacao_id;
 
 SELECT SUM(valor), tipo_transacao_id
 FROM cliente_transacoes
 GROUP BY tipo_transacao_id
+ORDER BY tipo_transacao_id ASC;
 
 SELECT SUM(valor), tipo_transacao_id
 FROM cliente_transacoes
 GROUP BY tipo_transacao_id
-ORDER BY tipo_transacao_id ASC
-
-SELECT SUM(valor), tipo_transacao_id
-FROM cliente_transacoes
-GROUP BY tipo_transacao_id
-ORDER BY tipo_transacao_id DESC
+ORDER BY tipo_transacao_id DESC;
